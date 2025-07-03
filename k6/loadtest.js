875af39e0475ac510bde39f73ac4b563d32cd845
Kevin Abrar Khansa <kevariable@gmail.com>
2025-07-03 20:49:35 +0700

    patch(franken): worker

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
@@ -9,12 +9,12 @@ export let options = {
     stages: [
         { duration: '1s', target: 5 },    // Warm up
         { duration: '5s', target: 50 },    // Quick spike to 50 users
-        { duration: '60s', target: 500 },   // Stay at spike (test PHP-FPM limits)
-        { duration: '5s', target: 400 },    // Drop back to normal
-        { duration: '10s', target: 300 },   // Normal load
+        { duration: '60s', target: 1000 },   // Stay at spike (test PHP-FPM limits)
+        { duration: '5s', target: 800 },    // Drop back to normal
+        { duration: '10s', target: 600 },   // Normal load
         { duration: '5s', target: 500 },   // Even bigger spike (stress test)
         { duration: '5s', target: 400 },   // Stay at big spike
-        { duration: '5s', target: 0 },     // Ramp down
+        { duration: '5s', target: 300 },     // Ramp down
     ],
     thresholds: {
       http_req_duration: ['p(95)<500'], // 95% of requests must complete below 500ms
@@ -22,13 +22,7 @@ export let options = {
 };
 
 export default function () {
-  // 2. Randomly select a blog page to hit
-  const pages = [
-    // 'https://localhost:443/en/blog/',
-    // 'https://localhost:443/en/blog/page/2',
-    'https://localhost:444/en/blog/page/3',
-  ];
-  const url = pages[Math.floor(Math.random() * pages.length)];
+  const url = 'https://localhost:443/en/blog/';
   const res = http.get(url);
 
   // 3. Basic checks
